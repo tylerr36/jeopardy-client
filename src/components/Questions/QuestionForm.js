5aed1294aabfa98f8c4526947db4abfcacf0fae2
TylerR36 <tylerrotman@gmail.com>
2019-12-04 18:28:55 -0500

    Styling changes in question update

--- src/components/Questions/QuestionForm.js
+++ src/components/Questions/QuestionForm.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const QuestionForm = ({ question, handleSubmit, handleChange, cancelPath }) => (
   <form onSubmit={handleSubmit}>
     <label htmlFor="question">Question</label>
+    <br/>
     <input
       id="question"
       placeholder="Enter/update question!"
@@ -11,8 +12,11 @@ const QuestionForm = ({ question, handleSubmit, handleChange, cancelPath }) => (
       name="question"
       onChange={handleChange}
     />
+    <br/>
+    <br/>
 
     <label htmlFor="answer">Answer</label>
+    <br/>
     <input
       id="answer"
       placeholder="Enter/update answer!"
@@ -20,6 +24,8 @@ const QuestionForm = ({ question, handleSubmit, handleChange, cancelPath }) => (
       name="answer"
       onChange={handleChange}
     />
+    <br/>
+    <br/>
 
     <button type="submit">Submit</button>
     <Link to={cancelPath}>
